e1bc2b56ececb973ea8e1b10a6786abb83df3e50
LomeshGupta <guptabittu06@gmail.com>
2023-11-21 16:04:08 +0530

    deleteuser

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -4,11 +4,24 @@ const { json } = require("body-parser");
 const bcrypt = require("bcryptjs");
 
 
-//get all users-------------------------------------------------
+//get all users----------------------------------------------
 
 const getUsers = asyncHandler(async (req, res) => {
     const users = await User.find();
     res.status(200).json(users);
+  });
+
+
+  //delete one user------------------------------------------
+  const deleteUser = asyncHandler(async (req, res) => {
+    const { username } = req.body
+    if (!username) {
+        res.status(400)
+        throw new Error("Please enter all required fields.");
+      }
+    const users = await User.deleteOne({username});
+    res.status(200);
+    res.send("Deleted Successflly");
   });
 
 
@@ -35,6 +48,7 @@ const salt  = await bcrypt.genSalt(10)
 const hashedpass = await bcrypt.hash(password, salt);
 
   //create new user
+  
   const user = await User.create({
     username,
     fullname,
@@ -71,5 +85,6 @@ const hashedpass = await bcrypt.hash(password, salt);
 
 module.exports = {
   registerUser,
-  getUsers
+  getUsers,
+  deleteUser
 };
--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { registerUser, getUsers } = require("../controllers/userController");
+const { registerUser, getUsers, deleteUser } = require("../controllers/userController");
 const router = express.Router();
 
 const app = express()
 
 app.use(express.json())
 
+router.post("/deleteuser",deleteUser);
 router.get("/getusers",getUsers);
 router.post("/register", registerUser);
 
