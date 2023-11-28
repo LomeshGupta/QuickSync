d6dfca38a7c9f0538bf87b42a241ad412121555e
LomeshGupta <guptabittu06@gmail.com>
2023-11-28 15:25:44 +0530

    newcommint

--- Server/server.js
+++ Server/server.js
@@ -12,12 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: "*",
-  allowed: "POST, GET, PUT",
 };
 
 //middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
