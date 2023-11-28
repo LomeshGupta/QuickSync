6fc797fa0fa66000fa964d346eb9c9352cfbc59e
LomeshGupta <guptabittu06@gmail.com>
2023-11-28 14:30:32 +0530

    new

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -15,6 +15,9 @@ const getUsers = asyncHandler(async (req, res) => {
 
 //delete one user------------------------------------------
 const deleteUser = asyncHandler(async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   const { username } = req.body;
   if (!username) {
     res.status(400);
