16ae81f7cd3a65999d036a40f7ca831adc01ea87
LomeshGupta <guptabittu06@gmail.com>
2023-11-28 15:20:35 +0530

    cors

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { registerUser, getUsers, deleteUser } = require("../controllers/userController");
 const router = express.Router();
+const cors = require("cors");
 
 const app = express()
-
+app.use(cors());
 app.use(express.json())
+
 
 router.post("/deleteuser",deleteUser);
 router.get("/getusers",getUsers);
--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: "*",
   allowed: "POST, GET, PUT",
-
 };
 
 //middlewares
