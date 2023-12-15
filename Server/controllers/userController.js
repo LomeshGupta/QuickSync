84dd4249f8c8151a23b1754941fbfb9833f25af7
LomeshGupta <guptabittu06@gmail.com>
2023-12-15 14:54:52 +0530

    login

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -157,7 +157,7 @@ const loginUser = asyncHandler(async (req, res) => {
       expires: new Date(Date.now() + 1000 * 86400), // 1 day
       sameSite: "none",
       secure: true,
-    });s
+    });
   }
   if (user && passwordIsCorrect) {
     const {
